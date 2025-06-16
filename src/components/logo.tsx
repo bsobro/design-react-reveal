928bfdf1b07f185c7ce17f64777e6f5152f3cfac
bsobro <bsobro@gmail.com>
2025-06-16 12:31:38 +0545

    img update and logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { UsersRound } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const Logo = () => {
   return (
+    <Link to="/">
     <div className="flex items-center flex-nowrap">
       
-        <UsersRound size= {24} stroke="currentColor" strokeWidth={2} className="text-track pb-[1px]"/>
+      
+        <img src="/public/img/logo.png" width={50} height={50} alt="" />
         <span className="ml-1 text-2xl font-bold text-blue-900">Track</span><span className="text-2xl text-track">Staff</span>
       
+      
     </div>
+    </Link>
   )
 }
 
